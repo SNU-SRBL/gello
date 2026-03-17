2f684557e26bb27988b32a3595d3f98e5723300a
FunkyFox159 <dongrami10@naver.com>
2026-03-17 15:16:02 +0900

    Inspire hand wrapper skeleton. Need to complete proximity.

---
+++ gello_HD/gello/robots/SRBL_Inspire_copy.py
@@ -0,0 +1,190 @@
+"""
+File containing the SRBL Inspire gripper class for controlling the gripper and obtaining sensor data.
+Modified from the file which was written for GELLO, for a general purpose wrapper.
+Written by Seongjun Koh (Soft Robotics and Bionics Lab, Seoul National University), based on the Inspire SDK documentation and sample code provided by the manufacturer.
+Last modification: March 17, 2026
+"""
+
+import serial
+import time
+
+INSPIRE_regdict = {
+    'ID'         : 1000,
+    'baudrate'   : 1001,
+    'mode'       : 1100,
+    'clearErr'   : 1003,
+    'forceClb'   : 1007,
+    'angleSet'   : 1040, # Use this to set finger position, units of 0.1 degrees, -1 for no change
+    'forceSet'   : 1046,
+    'speedSet'   : 1052,
+    'angleAct'   : 1064, # Use this to get finger position : angle Actual, units of 0.1 degrees
+    'forceAct'   : 1070,
+    'currAct'    : 1076, # Use this to get current data, mA
+    'errCode'    : 1082,
+    'statusCode' : 1088,
+    'temp'       : 1094,
+    'ip'         : 1700,
+    'actionSeq'  : 2160,
+    'actionRun'  : 2162, 
+    'sensorData' : 3000
+}
+
+SRBL_INSPIRE_FINGER_LIST = ['little', 'ring', 'middle', 'index', 'thumb bending', 'thumb rotation']
+
+SRBL_INSPIRE_FINGER_LOWER_LIMIT = [900, 900, 900, 900, 1100, 600] # Lower limit of the finger joint position, units of 0.1 degrees
+# 4 fingers : 900, thunmb bending : 1100, thumb rotation : 600
+SRBL_INSPIRE_FINGER_UPPER_LIMIT = [1740, 1740, 1740, 1740, 1350, 1800] # Upper limit of the finger joint position, units of 0.1 degrees
+# 4 fingers : 1740, thunmb bending : 1350, thumb rotation : 1800
+
+class SRBL_Inspire_gripper:
+    def __init__(self, device_name="/dev/ttyUSB1", baudrate=115200):
+        self.ser = serial.Serial(device_name, baudrate, timeout=0.1)
+        self._SRBL_initialize()
+
+    def __del__(self):
+        if self.ser and self.ser.is_open:
+            self.ser.close()
+
+    def close(self):
+        if self.ser and self.ser.is_open:
+            self.ser.close()
+    
+    def _writeRegister(self, id, add, num, val):
+        bytes = [0xEB, 0x90]            
+        bytes.append(id)                
+        bytes.append(num + 3)           
+        bytes.append(0x12)              
+        bytes.append(add & 0xFF)        
+        bytes.append((add >> 8) & 0xFF) 
+        for i in range(num):
+            bytes.append(val[i])
+        checksum = 0x00                 
+        for i in range(2, len(bytes)):
+            checksum += bytes[i]        
+        checksum &= 0xFF                
+        bytes.append(checksum)
+        
+        self.ser.write(bytes)                
+        time.sleep(0.01)                
+        self.ser.read_all() # flush the response
+    
+    def _readRegister(self, id, add, num, mute=True):
+        bytes = [0xEB, 0x90]            
+        bytes.append(id)                
+        bytes.append(0x04)              
+        bytes.append(0x11)              
+        bytes.append(add & 0xFF)        
+        bytes.append((add >> 8) & 0xFF) 
+        bytes.append(num)
+        checksum = 0x00                 
+        for i in range(2, len(bytes)):
+            checksum += bytes[i]        
+        checksum &= 0xFF                
+        bytes.append(checksum)          
+        
+        self.ser.write(bytes)           
+        time.sleep(0.01)                
+        recv = self.ser.read_all()      
+        if len(recv) == 0:              
+            return []
+        num = (recv[3] & 0xFF) - 3      
+        val = []
+        for i in range(num):
+            value = (recv[7 + i])
+            val.append(value)
+        if not mute:
+            print('Read:', end='')
+            for i in range(num):
+                print(val[i], end=' ')
+            print()
+        return val
+    
+    def _SRBL_bytes_to_int16(self, val):
+        if len(val) < 2:
+            raise ValueError("Not enough bytes to convert to int16")
+        value = val[0] + (val[1] << 8)
+        if value > 32767:
+            value -= 65536
+        return value
+
+    def get_position_values(self):
+        '''
+        Get position of the joints.
+            - Returns a list of 6 values corresponding to the 6 joints (little, ring, middle, index, thumb bending, thumb rotation).
+            - Each value is in degrees, obtained by reading the 'angleAct' register and converting from units of 0.1 degrees.
+        '''
+        val = self._readRegister(1, INSPIRE_regdict['angleAct'], 12, True)
+        if len(val) < 12:
+            raise RuntimeError("Failed to read gripper position")
+        val_act = []
+        for i in range(6):
+            value_act = self._SRBL_bytes_to_int16(val[i*2:(i*2)+2])
+            val_act.append(value_act)
+        pos = float(val_act[self.finger - 1]) / 10.0 # convert to degrees
+        return pos
+
+    def move_fingers(self, targets, limit=True):
+        '''
+        Move the joints to the target positions.
+            - targets: a list of 6 values corresponding to the target positions for the 6 joints (little, ring, middle, index, thumb bending, thumb rotation).
+            - limit: if True, the target positions will be clamped to the defined upper and lower limits for each joint. default = True
+            - Use '-1' for joints that should not be changed.
+        '''
+        if limit:
+            for i in range(6):
+                if targets[i] != -1:
+                    targets[i] = int(min(SRBL_INSPIRE_FINGER_UPPER_LIMIT[i], max(SRBL_INSPIRE_FINGER_LOWER_LIMIT[i], targets[i])))
+        val_reg = []
+        for i in range(6):
+            val_reg.append(targets[i] & 0xFF)
+            val_reg.append((targets[i] >> 8) & 0xFF)
+        self._writeRegister(1, INSPIRE_regdict['angleSet'], 12, val_reg)
+
+    def get_sensor_values(self, all=False):
+        # cf) 25 ms delay in the sample code, which is removed here
+        # 0-9: little / 0-1: normal, 2-3: tangential, 4-5: tangential direction, 6-9 : proximity
+        # 10-19: ring, 20-29: middle, 30-39: index, 40-49: thumb, 50-67: palm
+        sensor_vals = {}
+        if all:
+            # reads all 68 bytes of sensor data (5 fingers * 10 bytes + palm 3*6 bytes)
+            val = self._readRegister(1, INSPIRE_regdict['sensorData'], 68, True)
+            if len(val) < 68:
+                raise RuntimeError("Failed to read gripper sensor data")
+            for i in range(len(SRBL_INSPIRE_FINGER_LIST)):
+                idx = 10 * i
+                sensor_vals[SRBL_INSPIRE_FINGER_LIST[i]]['normal'] = self._SRBL_bytes_to_int16(val[idx:idx+2]) / 100.0 # convert to N
+                sensor_vals[SRBL_INSPIRE_FINGER_LIST[i]]['tangential'] = self._SRBL_bytes_to_int16(val[idx+2:idx+4]) / 100.0 # convert to N
+                sensor_vals[SRBL_INSPIRE_FINGER_LIST[i]]['tangential_dir'] = self._SRBL_bytes_to_int16(val[idx+4:idx+6])
+                sensor_vals[SRBL_INSPIRE_FINGER_LIST[i]]['proximity'] = val[idx+6:idx+10] # TODO: implement proximity data conversion
+        else:
+            val = self._readRegister(1, INSPIRE_regdict['sensorData'], 46, True) # read only the first 46 bytes for the 5 fingers
+            if len(val) < 46:
+                raise RuntimeError("Failed to read gripper sensor data")
+            for i in range(len(SRBL_INSPIRE_FINGER_LIST)):
+                idx = 10 * i
+                sensor_vals[SRBL_INSPIRE_FINGER_LIST[i]]['normal'] = self._SRBL_bytes_to_int16(val[idx:idx+2]) / 100.0 # convert to N
+                sensor_vals[SRBL_INSPIRE_FINGER_LIST[i]]['tangential'] = self._SRBL_bytes_to_int16(val[idx+2:idx+4]) / 100.0 # convert to N
+                sensor_vals[SRBL_INSPIRE_FINGER_LIST[i]]['tangential_dir'] = self._SRBL_bytes_to_int16(val[idx+4:idx+6])
+        return sensor_vals
+    
+    def get_current_values(self):
+        '''
+        Get current data for the joints.
+            - Returns a list of 6 values corresponding to the current for the 6 joints (little, ring, middle, index, thumb bending, thumb rotation).
+            - Each value is in Amperes, obtained by reading the 'currAct' register and converting from mA to A.
+        '''
+        val = self._readRegister(1, INSPIRE_regdict['currentData'], 12, True)
+        if len(val) < 12:
+            raise RuntimeError("Failed to read gripper current data")
+        current_vals = []
+        for i in range(6):
+            current_vals.append(self._SRBL_bytes_to_int16(val[i*2:(i*2)+2]) / 1000.0) # convert mA to A
+        return current_vals
+        # little, ring, middle, index, thumb bending, thumb rotation
+
+    def get_velocity_values(self):
+        """
+        Inspire does not provide velocity data, so this function is not implemented.
+        If needed, velocity can be estimated by numerical differentiation of position data.
+        """
+        pass
